c3e6d26cf0c4d885c5d4c712779a8a5c2fe228ca
cabboose <shayanftw@gmail.com>
2025-05-29 03:07:42 +0800

    Test 'works'

--- examples/src/App.fs
+++ examples/src/App.fs
@@ -1,9 +1,24 @@
 module App
 
 open Oxpecker.Solid
+open Oxpecker.Solid.MotionOne
+
+[<SolidComponent>]
+let FlipText (text: string) =
+    div(style = "display: flex") {
+        For(each = (text.ToCharArray() |> Array.map string)) {
+            yield fun letter index ->
+                Motion(
+                    tag = "h1",
+                    style = "transform-origin: bottom",
+                    animate = "{ rotateX: [0, 90, 180, 270, 360, 360], opacity: 1 }",
+                    transition = "{ duration: 2, offset: [0, 0.2, 0.3, 0.5, 0.8, 1], repeat: Infinity, easing: 'ease-in-out' }"
+                    ) { if letter = " " then "\u00a0" else letter }
+        }
+    }
 
 [<SolidComponent>]
 let App() =
     h1() {
-        "Hello world!"
+        FlipText "Hello world!"
     }
